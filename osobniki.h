78be66c2995c6daf33dbdd22f0480e5b9931ed2c
yethan <yethan444@gmail.com>
2025-05-28 16:03:24 +0200

    lab7.1

--- osobniki.h
+++ osobniki.h
@@ -4,11 +4,58 @@
 
 #ifndef OSOBNIKI_H
 #define OSOBNIKI_H
+#include "mieszkaniec.h"
+#include "organizm.h"
 
+#include "ustawienia.h"
+#include "generatorlosowy.h"
+#include "sasiedztwo.h"
 
+static const UstawieniaSymulacji & UST
+                    = UstawieniaSymulacji::pobierzUstawienia();
 
-class osobniki {
+class Glon: protected Organizm, public Mieszkaniec {
+public:
+    Glon();
+    virtual RodzajMieszkanca kimJestes() final;
 
+    virtual ZamiarMieszkanca
+                    wybierzAkcje(Sasiedztwo sasiedztwo) override;
+
+    virtual Mieszkaniec * dajPotomka() override;
+
+    virtual void przyjmijZdobycz(
+                            Mieszkaniec *mieszkaniec) override;
+};
+
+class Grzyb: protected Organizm, public Mieszkaniec
+{
+public:
+    Grzyb();
+    virtual RodzajMieszkanca kimJestes() final;
+
+    virtual ZamiarMieszkanca
+                        wybierzAkcje(Sasiedztwo sasiedztwo) override;
+
+    virtual Mieszkaniec * dajPotomka() override;
+
+    virtual void przyjmijZdobycz
+                            (Mieszkaniec * mieszkaniec);
+};
+
+class Bakteria: protected Organizm, public Mieszkaniec
+{
+public:
+    Bakteria();
+    virtual RodzajMieszkanca kimJestes() final;
+
+    virtual ZamiarMieszkanca
+                    wybierzAkcje(Sasiedztwo sasiedztwo) override;
+
+    virtual Mieszkaniec * dajPotomka() override;
+
+    virtual void przyjmijZdobycz
+                            (Mieszkaniec * mieszkaniec);
 };
 
 
