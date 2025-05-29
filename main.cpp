4b5b8204078eee01e6d2b00b5306cd7ad516e403
yethan <yethan444@gmail.com>
2025-05-29 09:10:35 +0200

    lab 7.3, test Mieszkaniec and Nisza in main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 #include "organizm.h"
 #include "ustawienia.h"
 #include "generatorlosowy.h"
 #include "sasiedztwo.h"
+#include "nisza.h"
+#include "osobniki.h"
 
-using namespace std;
+static Nisza n1, n2, n3;
+
+static char sep = UstawieniaSymulacji::
+        pobierzUstawienia().znakSeparator;
+
+void wyswietlNisze()
+{
+    cout << n1.jakisSymbol() << sep
+         << n2.jakisSymbol() << sep
+         << n3.jakisSymbol() << endl;
+}
 
 int main()
 {
+    cout << "Puste nisze: ";
+    wyswietlNisze();
 
+    cout << "Lokowanie mieszkańców: ";
+    n1.przyjmijLokatora(new Glon());
+    n3.przyjmijLokatora(new Grzyb());
+    wyswietlNisze();
+
+    cout << "Przesuwanie lokatorów: ";
+    n2 = n1;
+    wyswietlNisze();
+
+    cout << "Przesuwanie lokatorów:";
+    n3 = n2;
+    wyswietlNisze();
+
+    cout << endl;
     return 0;
 }
+
--- nisza.h
+++ nisza.h
@@ -1,14 +1,51 @@
-//
-// Created by yethan on 5/16/25.
-//
-
 #ifndef NISZA_H
 #define NISZA_H
+#include "mieszkaniec.h"
+#include "sasiedztwo.h"
 
 
+class Nisza
+{
+private:
+    Mieszkaniec * lokator;
+public:
+    Nisza();
+    Nisza(Nisza &innaNisza);
+    ~Nisza();
 
-class nisza {
+    Nisza& operator=(Nisza & innaNisza);
 
+    void przyjmijLokatora(Mieszkaniec *lokatorBezdomny);
+
+    Mieszkaniec *oddajLokatora();
+
+    bool zajeta() const {return lokator != nullptr;}
+
+    RodzajMieszkanca ktoTuMieszka()
+    {
+        return zajeta() ? lokator->kimJestes(): PUSTKA;
+    }
+
+    bool lokatorZywy() const;
+
+    char jakisSymbol() const;
+
+private:
+    ZamiarMieszkanca aktywujLokatora(
+                            Sasiedztwo sasiedztwo)
+    {
+        return lokator->wybierzAkcje(sasiedztwo);
+    }
+
+    Mieszkaniec * wypuscPotomka()
+    {
+        return lokator->dajPotomka();
+    }
+
+    void przyjmijZdobycz(Mieszkaniec * ofiara)
+    {
+        lokator->przyjmijZdobycz((ofiara));
+    }
 };
 
 
